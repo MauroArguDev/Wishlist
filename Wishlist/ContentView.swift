9158c1b4733a15747d75330b2082465d00505df0
Mauricio Argumedo <argudev19@gmail.com>
2025-09-28 17:29:39 -0600

    Preview sample data implemented

--- Wishlist/ContentView.swift
+++ Wishlist/ContentView.swift
@@ -17,6 +17,8 @@ struct ContentView: View {
             List {
                 ForEach(wishList) { wish in
                     Text(wish.title)
+                        .font(.title.weight(.light))
+                        .padding(.vertical, 2)
                 }
             }
             .navigationTitle("Wishlist")
@@ -29,7 +31,19 @@ struct ContentView: View {
     }
 }
 
-#Preview {
+#Preview("Sample Data") {
+    let container = try! ModelContainer(for: Wish.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+    
+    container.mainContext.insert(Wish(title: "Master SwiftData"))
+    container.mainContext.insert(Wish(title: "Buy a new iPhone"))
+    container.mainContext.insert(Wish(title: "Get Married"))
+    container.mainContext.insert(Wish(title: "Live in Nederland"))
+    
+    return ContentView()
+        .modelContainer(container)
+}
+
+#Preview("Empty List") {
     ContentView()
         .modelContainer(for: Wish.self, inMemory: true)
 }
